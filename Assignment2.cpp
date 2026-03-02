d3dc418ee76ea09ef9692f623a3952f75d47aa4d
Kelley, Devon <devon.kelley.r@gmail.com>
2026-03-02 11:29:02 -0800

    Finished Assignments

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main() {
+int main() 
+{
+//Title Name
 
-   /* Type your code here. */
+   cout<<"Sales Data of 5 Stores"<<endl<<endl;
+
+// Body
+
+   const int stores = 5;
+   double sales [stores];
+
+   for (int i=0; i<stores; i++)
+      {
+         cout<<"Enter today's sales for store #" << (i+1) <<": $";
+         cin>> sales[i];
+      }
+
+   cout<<"\nSales Chart\n";
+   
+   for (int i=0; i<stores; i++)
+      {
+         cout<<"Stores "<<(i+1) << ": ";
+
+         int stars = sales[i] / 100;
+
+         for (int j=0; j<stars; j++)
+            {
+               cout<<"*";
+            }
+         cout<<endl;
+      }
 
    return 0;
 }
--- Assignment3.1.cpp
+++ Assignment3.1.cpp
@@ -1,10 +1,31 @@
-// Chapter 5, Programming Challenge 17: Sales Bar Chart
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 int main()
 {
+
+// Title
+
+    cout<<"Two Interger Increments"<<endl<<endl;
+
+//Body
+
+    int start_num, end_num;
+    
+    cout<<"Choose two intergers (whole numbers):"<<endl;
+    cin>> start_num >> end_num;
+
+    cout<<endl;
+
+    if (start_num <= end_num)
+        {
+            for (int i = start_num; i <= end_num; i+=5)
+                {
+                    cout << i <<" ";
+                }
+            cout<<endl;
+        }
 
 return 0;
 }
